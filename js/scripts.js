6456ef8e7fcfc5fb6d9a0871c7344b02ad0542dc
Killian Brait <killianbrait@gmail.com>
2024-04-06 14:02:40 -0700

    bug-fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 // event listener for adjusting background gradient based on mouse position
 document.addEventListener('mousemove', function(e) {
-    const x = e.clientX / window.innerWidth * 100;
-    const y = e.clientY / window.innerHeight * 100;
+    const x = e.clientX; // / window.innerWidth * 100;
+    const y = e.clientY; // / window.innerHeight * 100;
     document.body.style.backgroundPosition = `${x}% ${y}%`;
   });
   
